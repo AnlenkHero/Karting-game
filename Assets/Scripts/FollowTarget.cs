ce48dbf6f53113ee5cf4e27547a6271b4aff0ad8
AnlenkHero <marchenko.roma2002@gmail.com>
2025-01-23 19:30:06 +0200

    Raw version of AudioManager

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Kart
+{
+    public class FollowTarget : MonoBehaviour
+    {
+        public Transform target;
+
+        private void Update()
+        {
+            if (target)
+            {
+                transform.position = target.position;
+            }
+        }
+    }
+}
