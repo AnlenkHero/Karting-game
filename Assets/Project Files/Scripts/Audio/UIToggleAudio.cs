6cc0b37975416328847e32b740a9ad22ff905293
AnlenkHero <marchenko.roma2002@gmail.com>
2025-04-02 21:56:54 +0300

    TmpDropdown and Toggle audio

---
+++ Assets/Project Files/Scripts/Audio/UIToggleAudio.cs
@@ -0,0 +1,31 @@
+﻿using Kart.Project_Files.Scripts.Managers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Kart.Project_Files.Scripts.Audio
+{
+    [RequireComponent(typeof(Toggle))]
+    public class UIToggleAudio : MonoBehaviour
+    {
+        [SerializeField] private Toggle toggle;
+        
+        private void Reset()
+        {
+            toggle = GetComponent<Toggle>();
+        }
+
+        private void Awake()
+        {
+            if (GetComponentInParent<Dropdown>() != null)
+            {
+                return;
+            }
+            
+            if (toggle == null)
+            {
+                toggle = GetComponent<Toggle>();
+            }
+            toggle.onValueChanged.AddListener((bool value) => AudioManager.Instance.PlayUI("clickUI"));
+        }
+    }
+}
