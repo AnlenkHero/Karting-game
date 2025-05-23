ed9a88b91949af4d1a978cdb5985bec3b72acad3
AnlenkHero <marchenko.roma2002@gmail.com>
2025-05-23 19:00:40 +0300

    Fixed slider sound + user settings ui

--- Assets/Project Files/Scripts/Audio/VolumeSlider.cs
+++ Assets/Project Files/Scripts/Audio/VolumeSlider.cs
@@ -1,4 +1,5 @@
-﻿using Kart.Project_Files.Scripts.Managers;
+﻿using System;
+using Kart.Project_Files.Scripts.Managers;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,9 +8,16 @@ namespace Kart.Project_Files.Scripts.Audio
     public class VolumeSlider : MonoBehaviour
     {
         [SerializeField] private Slider slider;
+        [SerializeField] private float sliderSoundThresholdTime = 0.1f;
         public string mixerParameter;
         public string mixerGroup;
         private float _lastVal;
+        private float _currentSliderSoundTime;
+
+        private void Update()
+        {
+            _currentSliderSoundTime += Time.deltaTime;
+        }
 
         private void OnEnable()
         {
@@ -17,10 +25,13 @@ namespace Kart.Project_Files.Scripts.Audio
             slider.onValueChanged.AddListener((val) =>
             {
                 if (Mathf.Approximately(val, _lastVal)) return;
-
-                AudioManager.Play("hoverUI", mixerGroup);
+                
                 _lastVal = val;
                 AudioManager.SetVolume(mixerParameter, val);
+                
+                if (!(_currentSliderSoundTime >= sliderSoundThresholdTime)) return;
+                AudioManager.Play("hoverUI", mixerGroup);
+                _currentSliderSoundTime = 0;
             });
         }
     }
